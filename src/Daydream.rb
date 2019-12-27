96721547f854cb576191235cee9babfe3ee27bb4
Donghee KIM <lastgleam@gmail.com>
2019-12-27 14:38:45 +0900

    add Daydream

---
+++ src/Daydream.rb
@@ -0,0 +1,22 @@
+# Daydream
+# https://atcoder.jp/contests/abc049/tasks/arc065_a
+#
+words = %w[dream dreamer erase eraser]
+text = gets.strip
+result = 'YES'
+
+until text.empty?
+  deleted = false
+  words.each do |w|
+    next unless text[-w.size, w.size] == w
+    text = text[0..-(w.size + 1)]
+    deleted = true
+  end
+  unless deleted
+    result = 'NO'
+    break
+  end
+end
+
+puts result
+
