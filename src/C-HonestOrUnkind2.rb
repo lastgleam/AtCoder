c523cba8130728ae6887ae21f704f22759bf2b7a
Donghee KIM <lastgleam@gmail.com>
2020-01-22 23:01:52 +0900

    Fix HonestOfUnkind2

--- src/C-HonestOrUnkind2.rb
+++ src/C-HonestOrUnkind2.rb
@@ -3,6 +3,7 @@
 #
 N = gets.strip.to_i
 evidences = []
+max = 0
 N.times do
   e = gets.strip.to_i
   one = []
@@ -11,22 +12,21 @@ N.times do
   end
   evidences.push one
 end
-count = 0
 
 [0,1].repeated_permutation(N).each do |kase|
   next unless kase.any?{ |n| n == 1 }
-  p kase
-  correct = true
-  evidences.each do |e|
-    e.each do |person, answer|
-      p "#{person}, #{answer}"
-      if kase[person] == 1 && kase[person - 1] != answer
-        correct = false
+  validated = false
+  evidences.each_with_index do |e, i|
+    e.each do |person, testimony|
+      if kase[i] == 1 && kase[person - 1] != testimony
+        validated = true
         break
       end
     end
+    break if validated
   end
-  p 'correct!' if correct
-  count += 1 if correct
+  next if validated
+  sum = kase.inject(:+)
+  max = sum if max < sum
 end
-puts count
+puts max
