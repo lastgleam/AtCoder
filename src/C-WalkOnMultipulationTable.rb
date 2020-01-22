d2282bfef743c5c60bb34bc301ca5347787ab386
Donghee KIM <lastgleam@gmail.com>
2020-01-23 01:58:42 +0900

    Add Walk On Multipulation Table

--- src/C-WalkOnMultipulationTable.rb
+++ src/C-WalkOnMultipulationTable.rb
@@ -1,12 +1,13 @@
-# B - 81
-# https://atcoder.jp/contests/abc144/tasks/abc144_b
+# C - Walk on Multiplication Table
+# https://atcoder.jp/contests/abc144/tasks/abc144_c
 #
+answer = 0
 num = gets.strip.to_i
-result = 'No'
-(1..9).each do |i|
-  if num % i == 0 && num / i < 10
-    result = 'Yes'
+max = Math.sqrt(num).to_i
+max.downto(1) do |n|
+  if num % n == 0
+    answer = n + (num / n) - 2
+    break
   end
 end
-puts result
-
+puts answer
