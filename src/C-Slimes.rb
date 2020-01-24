ef47b208fa3c188cd1aec7b856f30e54c07d625b
Donghee KIM <lastgleam@gmail.com>
2020-01-25 07:51:05 +0900

    Add Slimes

---
+++ src/C-Slimes.rb
@@ -0,0 +1,14 @@
+# C - Slimes
+# https://atcoder.jp/contests/abc143/tasks/abc143_c
+#
+N = gets.strip.to_i
+S = gets.strip.split('')
+a = S[0]
+count = 1
+(1..(N-1)).each do |i|
+  if a != S[i]
+    a = S[i]
+    count += 1
+  end
+end
+puts count
